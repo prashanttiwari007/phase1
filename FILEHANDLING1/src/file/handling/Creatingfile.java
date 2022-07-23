6e54b9dc278123a54fe57340a67b5680c9d6cb3c
prashanttiwari007 <109596924+prashanttiwari007@users.noreply.github.com>
2022-07-23 13:47:07 +0530

    commit it 1

---
+++ FILEHANDLING1/src/file/handling/Creatingfile.java
@@ -0,0 +1,35 @@
+package file.handling;
+
+import java.io.File;
+
+import java.io.IOException;
+
+public class Creatingfile {
+
+	public static void main(String[] args) {
+		// Import the File class
+		
+			
+
+				try {
+					File Obj = new File("myfile.txt");
+					if (Obj.createNewFile()) {
+						System.out.println("File created: "
+										+ Obj.getName());
+					}
+					else {
+						System.out.println("File already exists.");
+					}
+				}
+				catch (IOException e) {
+					System.out.println("An error has occurred.");
+					e.printStackTrace();
+				}
+			
+		
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
